aabfa30a2185f0f94d0494fdb4b0e24e9655244e
otakuto <otakuto893@gmail.com>
2017-09-17 08:37:38 +0900

    change date to when

--- db/migrate/20170916214053_create_events.rb
+++ db/migrate/20170916214053_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
       t.string :title
-      t.date :date
+      t.date :when
       t.string :where
       t.string :what
 
