a40323ecac87fdf9dfe157a2670de5a5d0285dd1
otakuto <otakuto893@gmail.com>
2017-09-17 13:18:07 +0900

    [update] seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,11 @@ Event.create(:title => "RUN! RUN! RUN!", :when => "2017-9-30T06:30", :where => "
 Event.create(:title => "A walk of the night.", :when => "2017-9-20T20:30", :where => "San Francisco", :what => "Walking")
 Event.create(:title => "Let's swim together!", :when => "2017-9-19T21:30", :where => "New York", :what => "Swimming")
 
+Organizer.create(:event_id => "1" , :user_id => "1")
+Participant.create(:event_id => "1" , :user_id => "2")
+
+Organizer.create(:event_id => "2" , :user_id => "3")
+Participant.create(:event_id => "2" , :user_id => "2")
+Participant.create(:event_id => "2" , :user_id => "4")
+
+Organizer.create(:event_id => "3" , :user_id => "4")
