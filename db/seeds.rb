d5a58e6d0ac86039ee93e378490fbf141aedd026
otakuto <otakuto893@gmail.com>
2017-09-17 10:34:21 +0900

    [fix] seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.create(:name => "Mahiro", :age => "21", :sex => "Male", :like_sport => "Swi
 User.create(:name => "Tact", :age => "21", :sex => "Male", :like_sport => "Bouldering", :email => "c@gmail.com", :password => "114514")
 User.create(:name => "Tom", :age => "20", :sex => "Male", :like_sport => "Soccer", :email => "d@gmail.com", :password => "114514")
 
-Event.create(:title => "Jogging", :when => "2017-9-31T06:30", :where => "Los Angels", :what => "Jogging")
+Event.create(:title => "Jogging", :when => "2017-9-30T06:30", :where => "Los Angels", :what => "Jogging")
 Event.create(:title => "Walking", :when => "2017-9-20T20:30", :where => "San Francisco", :what => "Walking")
 Event.create(:title => "Swimming", :when => "2017-9-19T21:30", :where => "New York", :what => "Swimming")
 
