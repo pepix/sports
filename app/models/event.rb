c3e56d07abe56282da53fefdb90525444179624b
otakuto <otakuto893@gmail.com>
2017-09-17 11:47:25 +0900

    [add] participants and organizers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,2 @@
 class Event < ApplicationRecord
-  has_one :host, foreign_key: "user_id", class_name: "User"
-  has_many :guests, foreign_key: "user_id", class_name: "User"
 end
---
+++ db/migrate/20170917021752_create_organizers.rb
@@ -0,0 +1,10 @@
+class CreateOrganizers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :organizers do |t|
+      t.integer :event_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
