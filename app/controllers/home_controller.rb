a1d0938493f3295e902bb5bd45472ab089a70052
otakuto <otakuto893@gmail.com>
2017-09-17 13:20:14 +0900

    [add] event join

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @events = Event.joins("INNER JOIN organizers ON organizers.event_id = events.id")
+               .joins("INNER JOIN users ON organizers.user_id = users.id")
+               .select("events.*, users.*")
+               .order(created_at: 'desc')
   end
 
   def show
